d44c77d71aa50dad35daf830f83ff66e6f9b6838
tongren <tongren@startdt.com>
2020-07-21 14:51:40 +0800

    :tada: init

---
+++ kirito-demo/src/main/java/com/kirito/cloud/KiritoDemoApplication.java
@@ -0,0 +1,11 @@
+package com.kirito.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiritoDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KiritoDemoApplication.class);
+    }
+}
